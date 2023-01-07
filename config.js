9a25c318a76699ee71e66e5ac800a605a3a6e770
YKYCYKYC <56754269+YKYCYKYC@users.noreply.github.com>
2023-01-07 17:01:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -66,12 +66,12 @@ var config = {
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "Go",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        play: "音乐起",
+        bannar_coming: "横幅上",
+        balloons_flying: "气氛组冲",
+        cake_fadein: "小蛋糕？大蛋糕！",
+        light_candle: "点火",
+        wish_message: "邹邹生日快乐",
         story: "A MESSAGE FOR YOU",
     }
 };
